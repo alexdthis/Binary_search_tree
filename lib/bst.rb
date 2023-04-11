ec4226dc681289e680698f9c85409fadefbd2fce
Alex Dang <94311646+alexdthis@users.noreply.github.com>
2023-04-10 18:42:06 -0700

    Add rest of the Tree class methods
    Added the level_order, depth, rebalance, and balanced? methods.
    Also reworked the insert and delete methods to catch some fringe
    errors. Added a driver function that tests the majority of the
    functions and to make sure the binary trees being made are good.

--- lib/bst.rb
+++ lib/bst.rb
@@ -2,7 +2,7 @@
 #This will take an array and then create a balanced binary search tree.
 
 #The lib file will consist of a Node class and a tree class. 
-#The tree class will store an array that contains a root value 
+#The tree class will store a root node that contains a root value 
 #along with two Node objects for the left and right subtree.
 
 class Tree
@@ -28,25 +28,24 @@ class Tree
 #creates a node with the specified value as the root node, then traverses the tree, and adds the node as a leaf node
 
     def insert(value, node = @tree)
-        if node.left_tree == nil && node.right_tree == nil
-            if node.root > value
-                node.left_tree = Node.new(value)
-                return nil
-            else
-                node.right_tree = Node.new(value)
-                return nil
-            end
-        end
-        if node.root > value
+        if node.root > value && node.left_tree != nil
             self.insert(value, node.left_tree)
-        else
+        elsif node.root > value && node.left_tree == nil
+            node.left_tree = Node.new(value)
+            return
+        end
+        if node.root < value && node.right_tree != nil
             self.insert(value, node.right_tree)
+        elsif node.root < value && node.right_tree == nil
+            node.right_tree = Node.new(value)
+            return
         end
     end
 
 #Traverses the tree and searches for the value
 #when the value is found, executes an inorder traversal in order to found the inorder successor of the node
-#then replaces it, afterwards, sets the deleted flag to true    
+#then replaces it, afterwards, sets the deleted flag to true
+#If the found node has only one child, moves that child up one level    
 
     def delete(value, deleted = false, node = @tree)
         if deleted && node != nil
@@ -72,11 +71,29 @@ class Tree
                 self.delete(value, deleted, node.right_tree)
             elsif node.root == value
                 if node.left_tree == nil && node.right_tree != nil
-                    node.root = node.right_tree
-                    node.right_tree = nil
+                    node.root = node.right_tree.root
+                    if node.right_tree.left_tree != nil
+                        node.left_tree = node.right_tree.left_tree
+                    else
+                        node.left_tree = nil
+                    end
+                    if node.right_tree.right_tree != nil
+                        node.right_tree = node.right_tree.right_tree
+                    else
+                        node.right_tree = nil
+                    end
                 elsif node.left_tree != nil && node.right_tree == nil
-                    node.root = node.left_tree
-                    node.left_tree = nil
+                    node.root = node.left_tree.root
+                    if node.left_tree.right_tree != nil
+                        node.right_tree = node.left_tree.right_tree
+                    else
+                        node.right_tree = nil
+                    end
+                    if node.left_tree.left_tree != nil
+                        node.left_tree = node.left_tree.left_tree
+                    else
+                        node.left_tree = nil
+                    end
                 elsif node.left_tree == nil && node.right_tree == nil
                     return
                 elsif node.left_tree != nil && node.right_tree != nil
@@ -171,21 +188,82 @@ class Tree
         end
     end
 
-#The Tree class will have the following methods:
 
 #4. level_order - accepts a block, traverses the tree in breadth-first level order, and yield 
 #each read node to the block. If no block is given, returns an array of values.
+#algo: take in a node
+#store it in an array
+#iterate over this array, storing each node's subtrees in the array
+#while also adding the node to another array
+#base condition is null and the array is empty
+
+    def level_order(temp_array = [@tree], output_array = [], &block)
+        if temp_array.empty?
+            return
+        end
+        working_array = temp_array
+        temp_array = []
+        working_array.each do |element|
+            if element != nil
+                if block_given?
+                    if block.call(element.root)
+                        output_array.push(element.root)
+                        temp_array.push(element.left_tree) unless element.left_tree == nil
+                        temp_array.push(element.right_tree) unless element.right_tree == nil
+                    else
+                        temp_array.push(element.left_tree) unless element.left_tree == nil
+                        temp_array.push(element.right_tree) unless element.right_tree == nil
+                    end
+                else
+                    output_array.push(element.root)
+                    temp_array.push(element.left_tree) unless element.left_tree == nil
+                    temp_array.push(element.right_tree) unless element.right_tree == nil
+                end
+            end
+        end
+        self.level_order(temp_array, output_array, &block)
+        return output_array
+    end
+    
+#height - accepts a node and returns distance from node to leaf node
+
+    def height(node = @tree)
+        if node == nil
+            return -1
+        end
+
+        left_height = self.height(node.left_tree)
+        right_height = self.height(node.right_tree)
+
+        if left_height > right_height
+            return left_height + 1
+        else
+            return right_height + 1
+        end
+    end
 	
-#6. height - accepts a node and returns distance from node to leaf node
+#depth - accepts a node and returns distance from node to root node
+
+    def depth(node = @tree)
+        return self.height(@tree) - self.height(node)
+    end
 	
-#7. depth - accepts a node and returns distance from node to root node
-	
-#8. balanced? - determines if difference of height of left and right subtrees are at most 1. If 
+#balanced? - determines if difference of height of left and right subtrees are at most 1. If 
 #not, it is not balanced.
+
+    def balanced?
+        return ((self.height(@tree.left_tree) - self.height(@tree.right_tree)) in (-1..1))
+    end
 	
-#9. rebalance - produces an array by traversing an unbalanced tree, then outputs it to the build-tree 
+#rebalance - produces an array by traversing an unbalanced tree, then outputs it to the build-tree 
 #method to make a new balanced binary search tree.
 
+    def rebalance
+        tree_to_rebalance = self.inorder
+        tree_to_rebalance.sort!
+        @tree = build_tree(tree_to_rebalance)
+        return @tree
+    end
 end
 
 class Node
@@ -198,13 +276,55 @@ class Node
     end
 end
 
+#driver program to test methods of the Tree class
+
+def driver_program(array)
+    puts "Random array #{array} has been inputted. Now building a binary search tree"
+    array.sort!
+    test_tree = Tree.new(array)
+    puts "Now testing if the search tree is balanced: #{test_tree.balanced?}"
+    puts "Printing out the tree in level order: #{test_tree.level_order.join('-')}"
+    puts "Printing out the tree in pre-order: #{test_tree.preorder.join('-')}"
+    puts "Printing out the tree in in-order: #{test_tree.inorder.join('-')}"
+    puts "Printing out the tree in post-order: #{test_tree.postorder.join('-')}"
+    puts "Now adding numbers to unbalance the tree..."
+    test_tree.insert(12312)
+    test_tree.insert(9999)
+    test_tree.insert(756)
+    test_tree.insert(88132)
+    puts "Is the binary tree balanced?: #{test_tree.balanced?}"
+    puts "Now rebalancing the tree..."
+    test_tree.rebalance
+    puts "Is the rebalanced binary tree balanced? #{test_tree.balanced?}"
+    puts "Printing out the new tree in level order: #{test_tree.level_order.join('-')}"
+    puts "Printing out the new tree in pre-order: #{test_tree.preorder.join('-')}"
+    puts "Printing out the new tree in in-order: #{test_tree.inorder.join('-')}"
+    puts "Printing out the new tree in post-order: #{test_tree.postorder.join('-')}"
+end
+
+test_array = (Array.new(15) {rand(1..100)})
+driver_program(test_array)
 
 
+#test_array = [1, 2, 3, 4, 5]
+#test_tree = Tree.new(test_array)
+#puts test_tree
+#print test_tree.preorder
+#test_tree.delete(5)
+#print test_tree.preorder
+#test_tree.insert(43)
+#test_tree.insert(234)
+#test_tree.insert(2342)
 
-test_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-test_tree = Tree.new(test_array)
-puts test_tree
-puts test_tree.find(7)
+#puts ''
+#print test_tree.preorder
+#puts ''
+#puts test_tree.balanced?
+#puts test_tree.rebalance
+#puts test_tree.balanced?
+#puts ''
+#print test_tree.preorder
+
 #test_block = Proc.new {|value| value > 4}
 #puts test_tree.inorder.join(' ')
 #test_tree.delete(5)
